920628c4360e97896c48e051d429d9e45bbcb3ee
EmNudge <emnudge@gmail.com>
2020-08-13 12:01:25 -0400

    fixed green and blue sliders being wrong colors

--- src/components/color-sliders/RgbSliders.svelte
+++ src/components/color-sliders/RgbSliders.svelte
@@ -9,6 +9,6 @@
 
 <div class="rgb">
   <ColorSlider color="#ff5739" bind:value={$r} />
-  <ColorSlider color="#41a5e1" bind:value={$g} />
-  <ColorSlider color="#69c33d" bind:value={$b} />
+  <ColorSlider color="#69c33d" bind:value={$g} />
+  <ColorSlider color="#41a5e1" bind:value={$b} />
 </div>
